b5430008033f1e75f8525eaa28365740601b5f31
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 18:14:37 -0500

    Create HandlerInterface.php

---
+++ src/HandlerInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/)
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Session;
+/**
+ * HandlerInterface
+ */
+interface HandlerInterface {
+    public function delete(string $name);
+    public function get(string $name, $defaultReturnValue = null);
+    public function set(string $name, $value);
+}
