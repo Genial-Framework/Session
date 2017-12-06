970ed7f7713a7a606ec120350dd10b26f0e481b6
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 21:20:03 -0500

    Create UnexpectedValueExceptionTest.php

---
+++ tests/UnexpectedValueExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/).
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Session\Tests;
+use PHPUnit\Framework\TestCase;
+use Genial\Session\Exception\UnexpectedValueException;
+/**
+ * UnexpectedValueExceptionTest
+ */
+final class UnexpectedValueExceptionTest extends TestCase {
+    /**
+     * exceptionTest()
+     *
+     * @throws UnexpectedValueException
+     *
+     * @return void
+     */
+    public function exceptionTest() {
+        $this->expectException(UnexpectedValueException::class);
+        throw new UnexpectedValueException();
+    }
+}
