34999b007fd0ae53dae7e78e85fe92114b51a468
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 21:09:22 -0500

    Create LengthExceptionTest.php

---
+++ tests/LengthExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/).
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Session\Tests;
+use PHPUnit\Framework\TestCase;
+use Genial\Session\Exception\LengthException;
+/**
+ * LengthExceptionTest
+ */
+final class LengthExceptionTest extends TestCase {
+    /**
+     * exceptionTest()
+     *
+     * @throws LengthException
+     *
+     * @return void
+     */
+    public function exceptionTest() {
+        $this->expectException(LengthException::class);
+        throw new LengthException();
+    }
+}
