b5370b66b4753597f06f1cb44e75a2e478cd555f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 21:11:37 -0500

    Create OutOfBoundsExceptionTest.php

---
+++ tests/OutOfBoundsExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/).
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Session\Tests;
+use PHPUnit\Framework\TestCase;
+use Genial\Session\Exception\OutOfBoundsException;
+/**
+ * OutOfBoundsException
+ */
+final class OutOfBoundsExceptionTest extends TestCase {
+    /**
+     * exceptionTest()
+     *
+     * @throws OutOfBoundsException
+     *
+     * @return void
+     */
+    public function exceptionTest() {
+        $this->expectException(OutOfBoundsException::class);
+        throw new OutOfBoundsException();
+    }
+}
