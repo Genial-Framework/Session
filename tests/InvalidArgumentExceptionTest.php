4e65a87cf5e32d05790554d86746245fb4d58a50
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 21:07:43 -0500

    Create InvalidArgumentExceptionTest.php

---
+++ tests/InvalidArgumentExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/).
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Session\Tests;
+use PHPUnit\Framework\TestCase;
+use Genial\Session\Exception\InvalidArgumentException;
+/**
+ * InvalidArgumentExceptionTest
+ */
+final class InvalidArgumentExceptionTest extends TestCase {
+    /**
+     * exceptionTest()
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return void
+     */
+    public function exceptionTest() {
+        $this->expectException(InvalidArgumentException::class);
+        throw new InvalidArgumentException();
+    }
+}
