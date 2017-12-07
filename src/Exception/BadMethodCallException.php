975f19268db858ca68dd81456c1672dd1414bd0e
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-06 20:02:34 -0500

    Clean code and fix block header

--- src/Exception/BadMethodCallException.php
+++ src/Exception/BadMethodCallException.php
@@ -1,18 +1,15 @@
 <?php
 /**
- * Genial Framework (https://genial.tech/php/genial-framework/).
+ * Genial Framework <https://nenglish.me/php/genial-framework>.
  *
- * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ * @link      <https://github.com/Genial-Framework/Genial-Framework> for the canonical source repository
  *
- * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
- * @license   https://genial.tech/license/new-bsd New BSD License
+ * @copyright Copyright (c) 2017-2017 Nicholas English <nenglish0820@outlook.com> <https://github.com/Nenglish7>
+ * @license   <https://nenglish.me/license/new-bsd> New BSD License
  */
 
 namespace Genial\Session\Exception;
 
-/**
- * BadMethodCallException.
- */
 class BadMethodCallException extends \BadMethodCallException implements ExceptionInterface
 {
 }
