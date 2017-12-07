c23da3a62b151cff63e9636d632fa616e9c28e32
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-06 20:10:02 -0500

    Clean code and fix block header

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,18 +1,15 @@
 <?php
 /**
- * Genial Framework (https://genial.tech/php/genial-framework/).
+ * Genial Framework <https://nenglish.me/php/genial-framework>.
  *
- * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ * @link      <https://github.com/Genial-Framework/Genial-Framework> for the canonical source repository
  *
- * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
- * @license   https://genial.tech/license/new-bsd New BSD License
+ * @copyright Copyright (c) 2017-2017 Genial Framework <https://github.com/Genial-Framework>
+ * @license   <https://nenglish.me/license/new-bsd> New BSD License
  */
 
 namespace Genial\Session;
 
-/**
- * HandlerInterface.
- */
 interface HandlerInterface
 {
     public function delete(string $name);
