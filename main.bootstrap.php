7e06550a1c52a9667749db9e813dfc88373861d8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 18:56:49 -0500

    Update main.bootstrap.php

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -10,4 +10,5 @@
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License
  */
+
 define('GENIAL_SESSION_DEPENDENT', true);
