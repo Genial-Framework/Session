9ef563a1c4ecb99c205e784df0bd1f5807738331
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 19:02:23 -0500

    Fix grammer

--- src/Handler.php
+++ src/Handler.php
@@ -5,10 +5,10 @@
  * @author    Nicholas English <https://github.com/Nenglish7>
  * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
  *
- * @link      <https://github.com/Genial-Framework/Session> for the canonical source repository
+ * @link      <https://github.com/Genial-Framework/Session> For the canonical source repository.
  *
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License
+ * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Session;
