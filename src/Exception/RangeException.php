22d99f25915142c4d5bef8fed30d6ef3ae109c23
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 19:09:55 -0500

    Update RangeException.php

--- src/Exception/RangeException.php
+++ src/Exception/RangeException.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * Genial Framework (https://genial.tech/php/genial-framework/).
+ * Genial Framework.
  *
- * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
  *
- * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
- * @license   https://genial.tech/license/new-bsd New BSD License
+ * @link      <https://github.com/Genial-Framework/Session> For the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Session\Exception;
