9eaadfd307ae755700f796714e8a965c0812de81
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 19:06:10 -0500

    Update HandlerInterface.php

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,20 +1,24 @@
 <?php
 /**
- * Genial Framework <https://nenglish.me/php/genial-framework>.
+ * Genial Framework.
  *
- * @link      <https://github.com/Genial-Framework/Genial-Framework> for the canonical source repository
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
  *
- * @copyright Copyright (c) 2017-2017 Genial Framework <https://github.com/Genial-Framework>
- * @license   <https://nenglish.me/license/new-bsd> New BSD License
+ * @link      <https://github.com/Genial-Framework/Session> For the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Session;
 
+/**
+ * HandlerInterface.
+ */
 interface HandlerInterface
 {
-    public function delete(string $name);
-
-    public function get(string $name, $defaultReturnValue = null);
-
-    public function set(string $name, $value);
+    public function delete(string $name = null);
+    public function get(string $name = null, $defaultReturnValue = null);
+    public function set(string $name = null, $value);
 }
