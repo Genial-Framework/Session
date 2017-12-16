1443132f0f3182f6c47aff1b4af4b5336c2756f3
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-15 21:36:44 -0500

    Update HandlerTest.php

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -1,1 +1,35 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Cookie> For the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cookie/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Session;
+
+use Genial\Session\Exception\RuntimeException;
+use Genial\Session\Exception\BadMethodCallException;
+use Genial\Session\Exception\UnexpectedValueException;
+
+/**
+ * HandlerTest
+ */
+final class HandlerTest extends \PHPUnit\Framework\TestCase
+{
+    private $handler;
+    function __construct()
+    {
+        $handler = new Handler(); 
+    }
+    public function testHandler()
+    {
+         $this->expectException(RuntimeException::class);
+         $this->handler->delete('test');
+    }
+}
