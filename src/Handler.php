2b18d2c568b3bc31a2c28ccb0e197c8466ac0fd4
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-20 01:53:04 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -1,146 +1,17 @@
 <?php
 /**
- * Genial Framework.
+ * LaxovePHP
  *
- * @author    Nicholas English <https://github.com/Nenglish7>
- * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ * @author Nicholas English <https://github.com/Nenglish7>
+ * @author LaxovePHP Contributors <https://github.com/orgs/LaxovePHP/people>
  *
- * @link      <https://github.com/Genial-Framework/Session> For the canonical source repository.
- *
- * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Session/blob/master/LICENSE> New BSD License.
+ * @copyright Copyright (c) 2018 LaxovePHP. <https://github.com/LaxovePHP>
+ * @license   <https://github.com/LaxovePHP/Validator/blob/master/LICENSE> New BSD License
  */
-
-namespace Genial\Session;
-
-use Genial\Session\Exception\RuntimeException;
-use Genial\Session\Exception\BadMethodCallException;
-use Genial\Session\Exception\UnexpectedValueException;
-
+namespace LaxovePHP\Session;
 /**
  * Handler.
  */
-class Handler extends Manager implements HandlerInterface
+class Handler extends Bridge implements HandlerInterface
 {
-    /**
-     * delete().
-     *
-     * Deletes a session variable.
-     *
-     * @param string|null $name The name of the session variable.
-     *
-     * @throws BadMethodCallException   If the `name` parameter is missing.
-     * @throws RuntimeException         If a session does not exist.
-     * @throws UnexpectedValueException If the `name` parameter is empty.
-     *
-     * @return void
-     */
-    public function delete(string $name = null)
-    {
-        if (! $this->exist()) {
-            throw new RuntimeException(sprintf(
-                '`%s` detects a non-existent session.',
-                __METHOD__
-            ));
-        }
-        if (is_null($name)) {
-            throw new BadMethodCallException(sprintf(
-                '`%s` expects the `$name` argument.',
-                __METHOD__
-            ));
-        }
-        $name = trim($name);
-        if (empty($name) || $name == '') {
-            throw new UnexpectedValueException(sprintf(
-                '`%s` expects `$name` to not be empty.',
-                __METHOD__
-            ));
-        }
-        if (isset($_SESSION[$name])) {
-            unset($_SESSION[$name]);
-        }
-    }
-
-    /**
-     * get().
-     *
-     * Gets a session variable.
-     *
-     * @param string|null $name               The name of the session variable.
-     * @param mixed|null  $defaultReturnValue The default return value if the
-     *                                        function fails.
-     *
-     * @throws BadMethodCallException   If the `name` parameter is missing.
-     * @throws RuntimeException         If a session does not exist.
-     * @throws UnexpectedValueException If the `name` parameter is empty.
-     *
-     * @return mixed Returns the value of the session variable or the default
-     *               return value.
-     */
-    public function get(string $name = null, $defaultReturnValue = null)
-    {
-        if (! $this->exist()) {
-            throw new RuntimeException(sprintf(
-                '`%s` detects a non-existent session.',
-                __METHOD__
-            ));
-        }
-        if (is_null($name)) {
-            throw new BadMethodCallException(sprintf(
-                '`%s` expects the `$name` argument.',
-                __METHOD__
-            ));
-        }
-        $name = trim($name);
-        if (empty($name) || $name == '') {
-            throw new UnexpectedValueException(sprintf(
-                '`%s` expects `$name` to not be empty.',
-                __METHOD__
-            ));
-        }
-        if (isset($_SESSION[$name])) {
-            return Utils::decode($_SESSION[$name]);
-        }
-
-        return $defaultReturnValue;
-    }
-
-    /**
-     * set().
-     *
-     * Sets a session variable.
-     *
-     * @param string|null $name  The name of the session variable.
-     * @param mixed       $value The value of the session variable.
-     *
-     * @throws BadMethodCallException   If the `name` parameter is missing.
-     * @throws RuntimeException         If a session does not exist.
-     * @throws UnexpectedValueException If the `name` parameter is empty.
-     *
-     * @return mixed Returns the value of the session variable or the default
-     *               return value.
-     */
-    public function set(string $name = null, $value)
-    {
-        if (! $this->exist()) {
-            throw new RuntimeException(sprintf(
-                '`%s` detects a non-existent session.',
-                __METHOD__
-            ));
-        }
-        if (is_null($name)) {
-            throw new BadMethodCallException(sprintf(
-                '`%s` expects the `$name` argument.',
-                __METHOD__
-            ));
-        }
-        $name = trim($name);
-        if (empty($name) || $name == '') {
-            throw new UnexpectedValueException(sprintf(
-                '`%s` expects `$name` to not be empty.',
-                __METHOD__
-            ));
-        }
-        $_SESSION[$name] = Utils::encode($value, false);
-    }
 }
